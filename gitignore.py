b9d7bfbc540a1e2d54a79c31fa105d3f14abf456
Drawserqzez <me@holton.lol>
2022-05-23 23:14:48 +0200

    Add comments

--- gitignore.py
+++ gitignore.py
@@ -4,9 +4,10 @@ import requests
 import sys
 
 try:
-    url = 'https://raw.githubusercontent.com/github/gitignore/main/'
+    url = 'https://raw.githubusercontent.com/github/gitignore/main/' # static link to github repo =)
+
     projectType = sys.argv[1]
-    url += projectType[0].upper() + projectType[1:] + '.gitignore'
+    url += projectType[0].upper() + projectType[1:] + '.gitignore' # Most of the time, the project's first letter is capitalised
 
     print("Getting gitignore file for project type " + projectType)
 
@@ -19,9 +20,9 @@ try:
     else:
         print('Invalid project type: ' + projectType)
 
-except IndexError as indexError: 
+except IndexError as indexError: # If sys.argv[1] doesn't exist
     print("You need to specify a language or a project type")
-except Exception as e: 
+except Exception as e: # any and all other errors
     print("Could not get the gitignore file :(")
     print(e)
 
